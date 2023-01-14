a6e67afa20a55175b84ae9ceb968c4c7899abf3e
tai-no-ala-ni <hideyukie25@gmail.com>
2023-01-14 23:58:38 +0900

    [fix] typo

--- vim/cellular-animation.lua
+++ vim/cellular-animation.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>ca", "<cmd>CellularAnimation make_it_rain<cr>")
+vim.keymap.set("n", "<leader>cam", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>cag", "<cmd>CellularAutomaton game_of_life<CR>")
